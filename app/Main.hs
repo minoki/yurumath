09b79ef81d3a430ec73c46b0ade5679bdc6dd7b7
ARATA Mizuki <minorinoki@gmail.com>
2018-05-17 14:29:50 +0900

    Implement conversion from math list to MathML

--- app/Main.hs
+++ app/Main.hs
@@ -7,23 +7,43 @@ import Text.YuruMath.TeX.State
 import Text.YuruMath.TeX.Tokenizer
 import Text.YuruMath.TeX.Expansion
 import Text.YuruMath.TeX.Execution
+import Text.YuruMath.TeX.Math
+import Text.YuruMath.TeX.PostMath
+import Text.YuruMath.Convert.TeXToMML
+import Text.YuruMath.Builder.MathML3
+import Text.Blaze.Renderer.Pretty as Pretty
 import Control.Monad.State.Strict
 import Control.Monad.Except
+import Control.Lens.Setter (modifying)
 import Data.OpenUnion
 
-tokenizeAll :: (MonadTeXState s m, MonadError String m, Value s ~ CommonValue, Expandable s ~ Union '[ConditionalMarkerCommand, CommonExpandable, CommonBoolean]) => m [TeXToken]
-tokenizeAll = do
-  t <- nextToken
-  case t of
-    Nothing -> return []
-    Just t -> (t:) <$> tokenizeAll
+type MathExpandableT = Union '[ConditionalMarkerCommand, CommonExpandable, CommonBoolean]
+type MathValue = Union '[CommonValue,MathStyleSet,MathAtomCommand,MathCommands,CommonExecutable]
+type MathLocalState = CommonLocalState MathExpandableT MathValue
+runMathList :: Bool -> String -> Either String MathList
+runMathList !isDisplay input = runExcept $ evalStateT action (initialMathState isDisplay $ initialState input)
+  where
+    action :: StateT (MathState MathLocalState) (Except String) MathList
+    action = do
+      modifying (localState . tsDefinitions)
+        $ \m -> mconcat [fmap Left expandableDefinitions
+                        ,fmap Right executableDefinitions
+                        ,fmap Right mathDefinitions
+                        ,m
+                        ]
+      runMMDGlobal <$> readMathMaterial defaultMathMaterialContext
 
 main :: IO ()
 main = do
-  input <- getContents
-  let tokenResult = runExcept (runStateT tokenizeAll (initialState input))
-  case tokenResult of
-    Left err -> putStrLn ("tokenize error: " ++ err)
-    Right (tokenList, _state) -> do
-      forM_ tokenList $ \t -> do
-        putStrLn (show t)
+  input <- getLine
+  case runMathList True input of
+    Left err -> putStrLn ("error: " ++ err)
+    Right mathList -> do
+      putStrLn ("math list: " ++ show mathList)
+      let mathList' = determineChoice DisplayStyle mathList
+          mathList'' = determineBinForm mathList'
+          mathList''' = pairOpenClose mathList''
+          mathList'''' = textSymbol mathList'''
+      putStrLn ("after some post-processing: " ++ show mathList'''')
+      let mml = math $ mconcat $ toMML DisplayStyle mathList''''
+      putStrLn $ "MathML: " ++ Pretty.renderMarkup mml
---
+++ src/Text/YuruMath/Convert/TeXToMML.hs
@@ -0,0 +1,63 @@
+module Text.YuruMath.Convert.TeXToMML where
+import Text.YuruMath.TeX.Types
+import Text.YuruMath.TeX.Math
+import Text.YuruMath.TeX.PostMath
+import Text.YuruMath.Builder.MathML3
+import Text.YuruMath.Builder.MathML3.Attributes as A
+
+toMML :: MathStyle -> MathList -> [MathML]
+toMML = doList
+  where
+    doList :: MathStyle -> MathList -> [MathML]
+    doList !style [] = []
+    doList style (IAtom atom : xs) = doAtom style atom : doList style xs
+    doList _ (IStyleChange style : xs) = doList style xs
+    doList style (IGenFrac gf num den : xs) = mfrac (mrow $ mconcat $ doList (smallerStyle style) num) (mrow $ mconcat $ doList (denominatorStyle style) den) : doList style xs
+    doList style (_ : xs) = doList style xs -- not implemented yet
+    doAtom :: MathStyle -> Atom -> MathML
+    doAtom style (atom@OpAtom { atomLimits = DisplayLimits })
+      -- TODO: mark nucleus as "movablelimits=true"
+      = let n = doNucleus style (atomType atom) (atomNucleus atom)
+            sub = doField (subscriptStyle style) (atomSubscript atom)
+            sup = doField (superscriptStyle style) (atomSuperscript atom)
+        in case (sub,sup) of
+             (Nothing, Nothing) -> n
+             (Just xs, Nothing) -> munder n (mrow $ mconcat xs)
+             (Nothing, Just xs) -> mover n (mrow $ mconcat xs)
+             (Just xs, Just ys) -> munderover n (mrow $ mconcat xs) (mrow $ mconcat ys)
+    doAtom style (atom@OpAtom { atomLimits = Limits })
+      -- TODO: mark nucleus as "movablelimits=false"
+      = let n = doNucleus style (atomType atom) (atomNucleus atom)
+            sub = doField (subscriptStyle style) (atomSubscript atom)
+            sup = doField (superscriptStyle style) (atomSuperscript atom)
+        in case (sub,sup) of
+             (Nothing, Nothing) -> n
+             (Just xs, Nothing) -> munder n (mrow $ mconcat xs)
+             (Nothing, Just xs) -> mover n (mrow $ mconcat xs)
+             (Just xs, Just ys) -> munderover n (mrow $ mconcat xs) (mrow $ mconcat ys)
+    -- doAtom style (atom@RadAtom {}) = _
+    -- doAtom style (atom@AccAtom {}) = _
+    doAtom style atom
+      = let n = doNucleus (nucleusStyle (atomType atom) style) (atomType atom) (atomNucleus atom)
+            sub = doField (subscriptStyle style) (atomSubscript atom)
+            sup = doField (superscriptStyle style) (atomSuperscript atom)
+        in case (sub,sup) of
+             (Nothing, Nothing) -> n
+             (Just xs, Nothing) -> msub n (mrow $ mconcat xs)
+             (Nothing, Just xs) -> msup n (mrow $ mconcat xs)
+             (Just xs, Just ys) -> msubsup n (mrow $ mconcat xs) (mrow $ mconcat ys)
+    doNucleus :: MathStyle -> AtomType -> MathField -> MathML
+    doNucleus !style !atomType MFEmpty = mrow mempty
+    doNucleus style AOrd (MFSymbol fam slot) = mi $ toMathML [slot]
+    doNucleus style AOrd (MFTextSymbol fam s) = mi $ toMathML s -- TODO: Handle numeric literal (<mn>)
+    doNucleus style atomType (MFSymbol fam slot) | atomType `elem` [AOp,ABin,ARel,AOpen,AClose,APunct]  = mo $ toMathML [slot]
+    doNucleus style atomType (MFTextSymbol fam s) | atomType `elem` [AOp,ABin,ARel,AOpen,AClose,APunct]  = mo $ toMathML s
+    doNucleus style atomType (MFSubList xs) = mrow $ mconcat $ doList style xs
+    doNucleus style atomType _ = mrow mempty -- not supported yet
+      -- movable limits
+    doField :: MathStyle -> MathField -> Maybe [MathML]
+    doField !style MFEmpty = Nothing
+    doField style (MFSymbol fam slot) = Just [mi $ toMathML [slot]] -- TODO: Handle mn
+    doField style (MFTextSymbol fam s) = Just [mi $ toMathML s]
+    doField style MFBox = Nothing -- ignored for now
+    doField style (MFSubList xs) = Just $ doList style xs
